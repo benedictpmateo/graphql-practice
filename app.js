28c5c5cacf97a433c7b7d002eb80dde0f136dabc
Benedict Mateo <benedictpmateo@gmail.com>
2020-03-02 16:59:40 +0800

    added mongoose

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/library', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,

29134d4c056b06bd4888709e7278f0301f45468f
benedictpmateo <46466530+benedictpmateo@users.noreply.github.com>
2020-03-04 09:25:46 +0800

    setup/build app (#1)
    * added mongoose
    
    * added mongo database

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors())
+
+mongoose.connect('mongodb://localhost:27017/library', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
